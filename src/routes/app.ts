ab11d3de21873892698def8245a01264a8a6e1a3
LEO22555 <lemartinezba@unal.edu.co>
2023-05-12 11:56:10 -0500

    utilizando tokens

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -1,9 +1,11 @@
 import express,{Application, Request, Response, NextFunction} from 'express'
+import passport from 'passport'
 import dotenv from 'dotenv'
 dotenv.config()
 
 import rutas_ejemplo from './rutas_ejemplo'
 import rutas_auth from './authRoutes'
+import miEstrategia from '../config/passport'
 
 //1. Crear un objeto express
 
@@ -11,7 +13,13 @@ const app:Application=express()
 
 //1.1 Emplear las rutas
 app.use('/auth', rutas_auth)
-app.use('/', rutas_ejemplo)
+
+//Poteger las siguientes rutas
+passport.use(miEstrategia)
+
+app.use(passport.initialize())
+app.use('/', passport.authenticate('jwt', {session:false}), rutas_ejemplo)
+
 
 //2. Respuesta cuando el recurso no existe 
 app.use ((req: Request, res:Response, next:NextFunction)=>{
